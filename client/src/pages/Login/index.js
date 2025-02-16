be88889c070de1a08cc381cc983bdc5f5aca223d
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-02-16 20:36:56 -0300

    Defiinicao da estrutura do HTML da pagina de login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import "./styles.css";
+import logo from "../../assets/logo.svg";
+import padlock from "../../assets/padlock.png";
 
 export default function login() {
-  return <h1>Bem vindo</h1>;
+  return (
+    <div className="login-container">
+      <section className="from">
+        <img src={logo} alt="logo" />
+        <form>
+          <h1>Access your Account</h1>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="password" />
+          <button type="submit">Login</button>
+        </form>
+      </section>
+      <img src={padlock} alt="padlock" />
+    </div>
+  );
 }
