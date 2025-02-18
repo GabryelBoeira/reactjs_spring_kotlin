3a0ad05e87a6600d0b13e5d408c679f64de44dcf
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-02-17 21:55:55 -0300

    Implementação do cabeçario da pagina de livros

--- client/src/pages/Book/index.js
+++ client/src/pages/Book/index.js
@@ -1,8 +1,24 @@
 import React from "react";
-import "./styles.css";
+//import "./styles.css";
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { FiPower } from "react-icons/fi";
 
-const Book = () => {
-  return <h1> Page Book</h1>;
-};
-
-export default Book;
+export default function Book() {
+  return (
+    <div className="book-container">
+      <header>
+        <img src={logo} alt="logo" />
+        <span>
+          Welcome <strong>Usuario</strong>
+        </span>
+        <Link className="button" to="book/new">
+          Add New Book
+        </Link>
+        <button type="button">
+          <FiPower size={18} color="#251fc5" />
+        </button>
+      </header>
+    </div>
+  );
+}
