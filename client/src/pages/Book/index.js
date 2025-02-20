73d058d2d38ab2b0edbe233cbe2e1c3f42075cb9
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-02-19 21:08:19 -0300

    adicionando o objeto de listagem dos livros na pagina de books

--- client/src/pages/Book/index.js
+++ client/src/pages/Book/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
-import { FiPower } from "react-icons/fi";
+import { FiPower, FiEdit, FiTrash2 } from "react-icons/fi";
+import getTranslation from "../../i18n.js";
 
 export default function Book() {
   return (
@@ -19,6 +20,75 @@ export default function Book() {
           <FiPower size={18} color="#251fc5" />
         </button>
       </header>
+
+      <h1>{getTranslation("book-list-register")}</h1>
+      <ul>
+        <li>
+          <strong>{getTranslation("title")}:</strong>
+          <p>Titulo livro</p>
+          <strong>{getTranslation("author")}:</strong>
+          <p>Escritor de livro</p>
+          <strong>{getTranslation("price")}:</strong>
+          <p>10,90</p>
+          <strong>{getTranslation("release-date")}:</strong>
+          <p>10/10/2025</p>
+          <button type="button">
+            <FiEdit size={20} color="251fc5" />
+          </button>
+          <button type="button">
+            <FiTrash2 size={20} color="251fc5" />
+          </button>
+        </li>
+
+        <li>
+          <strong>{getTranslation("title")}:</strong>
+          <p>Titulo livro</p>
+          <strong>{getTranslation("author")}:</strong>
+          <p>Escritor de livro</p>
+          <strong>{getTranslation("price")}:</strong>
+          <p>10,90</p>
+          <strong>{getTranslation("release-date")}:</strong>
+          <p>10/10/2025</p>
+          <button type="button">
+            <FiEdit size={20} color="251fc5" />
+          </button>
+          <button type="button">
+            <FiTrash2 size={20} color="251fc5" />
+          </button>
+        </li>
+        <li>
+          <strong>{getTranslation("title")}:</strong>
+          <p>Titulo livro</p>
+          <strong>{getTranslation("author")}:</strong>
+          <p>Escritor de livro</p>
+          <strong>{getTranslation("price")}:</strong>
+          <p>10,90</p>
+          <strong>{getTranslation("release-date")}:</strong>
+          <p>10/10/2025</p>
+          <button type="button">
+            <FiEdit size={20} color="251fc5" />
+          </button>
+          <button type="button">
+            <FiTrash2 size={20} color="251fc5" />
+          </button>
+        </li>
+        <li>
+          <strong>{getTranslation("title")}:</strong>
+          <p>Titulo livro</p>
+          <strong>{getTranslation("author")}:</strong>
+          <p>Escritor de livro</p>
+          <strong>{getTranslation("price")}:</strong>
+          <p>10,90</p>
+          <strong>{getTranslation("release-date")}:</strong>
+          <p>10/10/2025</p>
+          <button type="button">
+            <FiEdit size={20} color="251fc5" />
+          </button>
+          <button type="button">
+            <FiTrash2 size={20} color="251fc5" />
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
