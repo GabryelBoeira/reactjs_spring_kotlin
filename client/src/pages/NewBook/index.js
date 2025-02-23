8d2028c7c556c7b7520745722463b03c7e748a2f
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-02-23 20:12:02 -0300

    Adicionando integração ao cadastrar u novo livro

--- client/src/pages/NewBook/index.js
+++ client/src/pages/NewBook/index.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 import getTranslation from "../../i18n.js";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import api from "../../services/api.js";
 
 export default function NewBook() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [price, setPrice] = useState(0);
+  const [launchDate, setLaunchDate] = useState("");
+  const history = useHistory();
+  const accessToken = localStorage.getItem("accessToken");
+  const authorization = { headers: { Authorization: `Bearer ${accessToken}` } };
+
+  async function createNewBook(e) {
+    e.preventDefault();
+
+    const data = {
+      author,
+      launchDate,
+      price,
+      title,
+    };
+
+    try {
+      await api.post("/api/book/v1", data, authorization);
+      history.push("/books");
+    } catch (err) {
+      alert(getTranslation("login-failed"));
+    }
+  }
+
   return (
     <div className="new-book-container">
       <div className="content">
@@ -17,11 +44,28 @@ export default function NewBook() {
             <FiArrowLeft size={16} color="#251fc5" /> {getTranslation("back")}
           </Link>
         </section>
-        <form>
-          <input placeholder={getTranslation("title")} />
-          <input placeholder={getTranslation("author")} />
-          <input placeholder={getTranslation("price")} />
-          <input placeholder={getTranslation("release-date")} type="date" />
+        <form onSubmit={createNewBook}>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder={getTranslation("title")}
+          />
+          <input
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            placeholder={getTranslation("author")}
+          />
+          <input
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            placeholder={getTranslation("price")}
+          />
+          <input
+            value={launchDate}
+            onChange={(e) => setLaunchDate(e.target.value)}
+            placeholder={getTranslation("release-date")}
+            type="date"
+          />
           <button className="button" type="submit">
             {getTranslation("register")}
           </button>
