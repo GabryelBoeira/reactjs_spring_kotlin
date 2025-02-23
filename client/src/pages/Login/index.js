16c2d138d3d8d24ecd1a729cdc1e8c4b1f9134a1
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-02-23 19:22:59 -0300

    Realizando a integração do login com servidor

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 import "./styles.css";
+import api from "../../services/api";
 import logo from "../../assets/logo.svg";
 import padlock from "../../assets/padlock.png";
 import getTranslation from "../../i18n.js";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    const data = {
+      username,
+      password,
+    };
+
+    try {
+      const response = await api.post("/auth/signin", data);
+      localStorage.setItem("username", username);
+      localStorage.setItem("accessToken", response.data.accessToken);
+      localStorage.setItem("refreshToken", response.data.refreshToken);
+
+      history.push("/books");
+    } catch (err) {
+      alert(getTranslation("login-failed"));
+    }
+  }
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={logo} alt="logo" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>{getTranslation("access-app")}</h1>
-          <input type="text" placeholder={getTranslation("username")} />
-          <input type="password" placeholder={getTranslation("password")} />
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            placeholder={getTranslation("username")}
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder={getTranslation("password")}
+          />
           <button className="button" type="submit">
             {getTranslation("login")}
           </button>
